6e4ac02957379eb084e691108e7f0a42304d5c62
Kenneth Ceglia <KPZNet@outlook.com>
2022-09-01 13:13:49 -0600

    dev

--- peloton_data.py
+++ peloton_data.py
@@ -2,5 +2,5 @@ import PyPelo
 
 if __name__ == '__main__':
     status, userID, sessionID = PyPelo.PeloLogin('KenCeglia@hotmail.com', 'Tyrant@12k')
-    status, rideList = PyPelo.DumpRidesToJSONFile(userID, sessionID, 2)
+    status, rideList = PyPelo.DumpRidesToJSONFile(userID, sessionID, 25)
     print(f"Complete Successfully : {status}")
