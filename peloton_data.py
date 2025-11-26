6789a99e99f2ea3b05d2628cefd432e6e2259977
KennethCeglia <191412790+204028830_bhgithub@users.noreply.github.com>
2025-11-25 21:46:00 -0700

    branch b change

--- peloton_data.py
+++ peloton_data.py
@@ -1,7 +1,11 @@
 import PyPelo
 
+# branch change for B
 
 if __name__ == '__main__':
     status, userID, sessionID = PyPelo.PeloLogin('KenCeglia@hotmail.com', 'Tyrant@12k')
     status, rideList = PyPelo.DumpRidesToJSONFile(userID, sessionID, 5)
     print(f"Complete Successfully : {status}")
+
+
+# PPelo changes sun 719
