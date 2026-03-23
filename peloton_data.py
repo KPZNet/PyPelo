bf87365011157affb84cfd46bd7bec69237c5aca
Ceglia, Ken <Kenneth.Ceglia+bhgithub@bakerhughes.com>
2026-03-23 12:05:57 -0600

    Update branch_a - 2026-03-23

--- peloton_data.py
+++ peloton_data.py
@@ -5,3 +5,8 @@ if __name__ == '__main__':
     status, userID, sessionID = PyPelo.PeloLogin('KenCeglia@hotmail.com', 'Tyrant@12k')
     status, rideList = PyPelo.DumpRidesToJSONFile(userID, sessionID, 5)
     print(f"Complete Successfully : {status}")
+
+
+    # local change....
+
+    
